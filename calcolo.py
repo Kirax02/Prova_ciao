fd81b5a086c9cc965f7b7004b0f01a092ce405c6
Kirax02 <lory02.mo@gmail.com>
2024-10-16 10:48:18 +0200

    aggiunta calcolo

---
+++ calcolo.py
@@ -0,0 +1,5 @@
+base = float(input("inserie la base del triangolo: "))
+altezza = float(input( "inserire l'altezza del trinagolo: "))
+area = base * altezza / 2
+
+print(f"l area del triangolo è: {area}")
